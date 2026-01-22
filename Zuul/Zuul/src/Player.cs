072296eb5d892f8bbf62b2692926081412d14f77
snolli3 <keegstranoor@outlook.com>
2026-01-22 14:22:11 +0100

    added player health & damage

--- Zuul/Zuul/src/Player.cs
+++ Zuul/Zuul/src/Player.cs
@@ -2,10 +2,40 @@ class Player
 {
     // auto property
     public Room CurrentRoom { get; set; }
-
+    // fields
+    private int health;
     // constructor
     public Player()
     {
         CurrentRoom = null;
+        health = 100;
+    }
+    
+    
+    // methods
+    ... Damage( amount) { /*...*/ } // speler verliest health
+    ... Heal(... amount) { /*...*/ } // speler krijgt health
+    ... IsAlive() { /*...*/ } // checkt of speler nog leeft
+
+    public int Damage(int amount)
+    {
+        health = health - amount;
+        return health;
+    }
+
+    public int Heal(int amount)
+    {
+        health = health + amount;
+        return health;
+    }
+    
+    public bool IsAlive()
+    {
+        bool death = false;
+        if (health <= 0)
+        {
+            death = true;
+        }
+        return death
     }
 }
